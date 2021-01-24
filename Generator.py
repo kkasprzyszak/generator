5f3469bf8f704d5a4abdff8ab6dd19dba4470f88
Katarzyna Kasprzyszak <kkasp24@gmail.com>
2021-01-24 13:46:01 +0100

    Update

--- Generator.py
+++ Generator.py
@@ -7,8 +7,6 @@ from kafka import KafkaProducer
 import xml.etree.ElementTree as etree
 from datetime import datetime
 
-
-
 def publishPostsToKafka(postsFilePath, address, topic, delay):
     producer = KafkaProducer(bootstrap_servers=[f'{address}:9092'],
                              value_serializer=lambda x: json.dumps(x).encode('utf-8'),
@@ -19,7 +17,6 @@ def publishPostsToKafka(postsFilePath, address, topic, delay):
     data = etree.parse(postsFilePath)
     root = data.getroot()
 
-    #lastCreationDate = datetime.strptime('2013-07-19T06:17:13.607', '%Y-%m-%dT%H:%M:%S.%f')
     cnt = 0;
     try:
         for elem in root:
@@ -28,12 +25,6 @@ def publishPostsToKafka(postsFilePath, address, topic, delay):
                message[key] = value
 
             #print(json.dumps(message))
-            #logger.info(json.dumps(message))
-
-            creationDate = datetime.strptime(message["CreationDate"], '%Y-%m-%dT%H:%M:%S.%f')
-
-            #if creationDate <= lastCreationDate:
-            #    continue;
 
             producer.send(topic, value=message)
             cnt += 1
@@ -41,8 +32,6 @@ def publishPostsToKafka(postsFilePath, address, topic, delay):
                 logger.info('[Posts] Sent {0} messages'.format(cnt))
                 logger.info(json.dumps(message))
 
-            #if cnt > 30:
-            #   break;
             sleep(delay)
         logger.info('[Posts] Producer completed')
     except KeyboardInterrupt:
@@ -72,6 +61,6 @@ args = parser.parse_args()
 postsFilePath = args.filePath
 logger.info('[Posts] File path: {0}, Kafka address: {1}, Topic name: {2}'.format(postsFilePath, args.kafkaAddress, args.topicName))
 
-t2 = threading.Thread(name='2', target=publishPostsToKafka, args=[postsFilePath, args.kafkaAddress, args.topicName, 0.001])
+t2 = threading.Thread(name='2', target=publishPostsToKafka, args=[postsFilePath, args.kafkaAddress, args.topicName, 0.1])
 
 t2.start()
